8efccbf01b62f0510aac4e4dbd134d398b8a6049
abstrxtInfinity <anantverma2001@gmail.com>
2022-07-05 14:14:31 +0530

    added blog data

--- src/Data/BlogData.js
+++ src/Data/BlogData.js
@@ -1,5 +1,75 @@
-
 export const BlogData = [
+    {
+        title: `The Future of the Web Development`,
+        tags: `Web, Development, Technology`,
+        blog: `Web development simply means the process of creating and maintaining websites and includes various facets like web design, web programming, database management, and web publishing. To put it in a nutshell, the answer to “what is web development?” is that it includes all the activities related to the creation of applications that function on the internet – that is, websites.
+There are two main aspects of web development: Frontend and Backend.
+Frontend Development
+The frontend is the part of the website with which the end-user interacts; what they see, click on, the forms they submit, and so on. It uses:
+HTML is used to design the frontend using markup language – it’s the website structure.
+CSS is a simple language used to render the web pages presentable
+JavaScript is a scripting language that renders the websites dynamic
+Bootstrap is an open-source, free suite of tools that makes websites and web apps responsive or mobile friendly.`,
+        imageUrl:
+            "https://webandcrafts.com/blog/wp-content/uploads/2021/12/The-Future-and-Scope-of-Web-Development-Latest-Trends-to-Watch-out-for.jpg",
+        author: "Webandcrafts",
+    },
+    {
+        title: `What is a Blog? Definition, Blog Types, and Benefits Explained`,
+        tags: `Blogging`,
+        blog: `A blog is a web log, an online journal where the owner presents information in reverse chronological order. It can be a section within a website or a standalone project.
+When blogs first emerged around 1994, people used them mainly to share their personal lives, including their experiences and interests. Now, blogs have become an essential online marketing strategy for businesses and one of the most profitable career choices for individuals.
+The interface of blogs has changed significantly over time. Bloggers can now integrate various widgets and plugins into their platforms, such as a social media feed, a contact form, or an author profile.`,
+        imageUrl: '',
+        author: 'Maulidina M'
+    },
+    {
+        title: `The Future of the Web Development`,
+        tags: `Web, Development, Technology`,
+        blog: `Web development simply means the process of creating and maintaining websites and includes various facets like web design, web programming, database management, and web publishing. To put it in a nutshell, the answer to “what is web development?” is that it includes all the activities related to the creation of applications that function on the internet – that is, websites.
+There are two main aspects of web development: Frontend and Backend.
+Frontend Development
+The frontend is the part of the website with which the end-user interacts; what they see, click on, the forms they submit, and so on. It uses:
+HTML is used to design the frontend using markup language – it’s the website structure.
+CSS is a simple language used to render the web pages presentable
+JavaScript is a scripting language that renders the websites dynamic
+Bootstrap is an open-source, free suite of tools that makes websites and web apps responsive or mobile friendly.`,
+        imageUrl:
+            "https://www.onlinecoursereport.com/wp-content/uploads/2020/07/shutterstock_394793860-1024x784.jpg",
+        author: "Webandcrafts",
+    },
+    {
+        title: `What is a Blog? Definition, Blog Types, and Benefits Explained`,
+        tags: `Blogging`,
+        blog: `A blog is a web log, an online journal where the owner presents information in reverse chronological order. It can be a section within a website or a standalone project.
+When blogs first emerged around 1994, people used them mainly to share their personal lives, including their experiences and interests. Now, blogs have become an essential online marketing strategy for businesses and one of the most profitable career choices for individuals.
+The interface of blogs has changed significantly over time. Bloggers can now integrate various widgets and plugins into their platforms, such as a social media feed, a contact form, or an author profile.`,
+        imageUrl: 'https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_1024,h_576/https://www.iimtindia.net/Blog/wp-content/uploads/2021/05/Blogging-1024x576.jpg',
+        author: 'Maulidina M'
+    },
+    {
+        title: `What is a Blog? Definition, Blog Types, and Benefits Explained`,
+        tags: `Blogging`,
+        blog: `A blog is a web log, an online journal where the owner presents information in reverse chronological order. It can be a section within a website or a standalone project.
+When blogs first emerged around 1994, people used them mainly to share their personal lives, including their experiences and interests. Now, blogs have become an essential online marketing strategy for businesses and one of the most profitable career choices for individuals.
+The interface of blogs has changed significantly over time. Bloggers can now integrate various widgets and plugins into their platforms, such as a social media feed, a contact form, or an author profile.`,
+        imageUrl: '',
+        author: 'Maulidina M'
+    },
+    {
+        title: `The Future of the Web Development`,
+        tags: `Web, Development, Technology`,
+        blog: `Web development simply means the process of creating and maintaining websites and includes various facets like web design, web programming, database management, and web publishing. To put it in a nutshell, the answer to “what is web development?” is that it includes all the activities related to the creation of applications that function on the internet – that is, websites.
+There are two main aspects of web development: Frontend and Backend.
+Frontend Development
+The frontend is the part of the website with which the end-user interacts; what they see, click on, the forms they submit, and so on. It uses:
+HTML is used to design the frontend using markup language – it’s the website structure.
+CSS is a simple language used to render the web pages presentable
+JavaScript is a scripting language that renders the websites dynamic
+Bootstrap is an open-source, free suite of tools that makes websites and web apps responsive or mobile friendly.`,
+        imageUrl:
+            "https://www.freecodecamp.org/news/content/images/2020/04/screely-1586183781361.png",
+        author: "Webandcrafts",
+    },
 
-]
-
+];
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,31 +1,30 @@
-import BlogTile from '../../Components/BlogTile/BlogTile'
-import { BlogData } from '../../Data/BlogData'
-import './home.css'
+import BlogTile from "../../Components/BlogTile/BlogTile";
+import { BlogData } from "../../Data/BlogData";
+import "./home.css";
 
 function Home() {
-    return (
-        <>
-            <section className="blog-section">
-                <div className="heading">
-                    <h2>Here is where we tell our stories</h2>
-                </div>
-                <div className="blogs">
-                    {
-                        BlogData.map((item, index) => {
-                            
-                            return (
-                                
-                                    <BlogTile key={index} title={item.title} tags={item.tags} blogDescription={item.blog} imageUrl={item.imageUrl} author={item.author} id={index}/>
-                            
-                                
-                            )
-                        })
-                    }
-                </div>
-            </section>
-
-        </>
-    )
+  return (
+    <section className="blog-section">
+      <div className="heading">
+        <h2>Here is where we tell our stories</h2>
+      </div>
+      <div className="blogs">
+        {BlogData.map((item, index) => {
+          return (
+            <BlogTile
+              key={index}
+              title={item.title}
+              tags={item.tags}
+              blogDescription={item.blog}
+              imageUrl={item.imageUrl}
+              author={item.author}
+              id={index}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
